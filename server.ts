207002469911840a30f069aa3b7da0a0a838246a
ahmedmsayeem <msayeemahmed76@gmail.com>
2025-10-25 17:27:26 +0530

    render

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import db from './db';
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('static'));
